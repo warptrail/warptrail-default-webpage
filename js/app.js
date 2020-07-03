f485abd090072f5ec409279ea290e778589d9a21
Ryan Whitmore <ryan@whitmorespace.com>
2020-07-03 11:42:43 -0700

    first commit, uploading default webpage

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const greeting = "Greetings traveler. Welcome to the Web.";
+function displayMessage(message) {
+    console.log(message);
+}
+
+displayMessage(greeting);
+
+// reduce gives you an accumulator and a value in your callback function
+
